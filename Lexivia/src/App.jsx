cd049463cc3b0e41418f56ce5c5a10481d713073
Ala Bensabra <ala.bensabra@ensia.edu.dz>
2026-04-24 01:52:48 +0100

    fix merge

--- Lexivia/src/App.jsx
+++ Lexivia/src/App.jsx
@@ -1,23 +1,35 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import Competitions from "./pages/Competitions";
 import CreateCompetition from "./pages/CreateCompetition";
 import CompetitionDetails from "./pages/CompetitionDetails";
+
 import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/competitions" element={<Competitions />} />
-          <Route path="/competitions/:competitionId" element={<CompetitionDetails />} />
-          <Route path="/create-competition" element={<CreateCompetition />} />
-        </Routes>
-      </BrowserRouter>
-      );
+      <Routes>
+        {/* Redirect root */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+        {/* Main pages */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/competitions" element={<Competitions />} />
+        <Route path="/create-competition" element={<CreateCompetition />} />
+
+        {/* Competition details (VERY IMPORTANT) */}
+        <Route
+          path="/competitions/:competitionId"
+          element={<CompetitionDetails />}
+        />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-      export default App;
+export default App;
